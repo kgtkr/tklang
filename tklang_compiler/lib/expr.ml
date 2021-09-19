5653558b0db7e9987dc47590d9c0cf40d0ab21fc
kgtkr <kgtkr.jp@gmail.com>
2021-09-19 22:12:21 +0900

    識別子にidを割り振るコード

--- tklang_compiler/lib/expr.ml
+++ tklang_compiler/lib/expr.ml
@@ -1,3 +1,5 @@
+open Map_ext
+
 type
     op = Add | Sub | Mul | Lt
     and 'id pat = AnyPat of 'id | EmptyListPat | ConsPat of 'id pat * 'id pat | IgnorePat
@@ -15,6 +17,69 @@ type
     | EmptyList of 'a
     | Cons of 'a * ('id, 'a) t * ('id, 'a) t
     | Match of 'a * ('id, 'a) t * ('id, 'a) clause list;;
+
+let rec id_string_to_int_pat (pat: string pat) (counter: int): (int pat * int MS.t * string MI.t * int) =
+    match pat with
+    | AnyPat id ->
+        let (id, env, s, counter) = (counter, MS.singleton id counter, MI.singleton counter id, counter + 1) in
+        (AnyPat id, env, s, counter)
+    | EmptyListPat ->
+        (EmptyListPat, MS.empty, MI.empty, counter)
+    | ConsPat(pat1, pat2) ->
+        let (pat1, env1, s1, counter) = id_string_to_int_pat pat1 counter in
+        let (pat2, env2, s2, counter) = id_string_to_int_pat pat2 counter in
+        (ConsPat(pat1, pat2), env1 |> MS.add_seq (MS.to_seq env2), s1 |> MI.add_seq (MI.to_seq s2), counter)
+    | IgnorePat ->
+        (IgnorePat, MS.empty, MI.empty, counter)
+
+let rec id_string_to_int (env: int MS.t) (e: (string, 'a) t) (counter: int): ((int, 'a) t * string MI.t * int) =
+    match e with
+        | Int (m, a) -> (Int (m, a), MI.empty, counter)
+        | Bool (m, a) -> (Bool (m, a), MI.empty, counter)
+        | Var (m, id) -> (Var (m, MS.find id env), MI.empty, counter)
+        | Op (m, e1, a, e2) ->
+            let (e1, s1, counter) = id_string_to_int env e1 counter in
+            let (e2, s2, counter) = id_string_to_int env e2 counter in
+            (Op (m, e1, a, e2), MI.add_seq (MI.to_seq s2) s1, counter)
+        | If (m, e1, e2, e3) ->
+            let (e1, s1, counter) = id_string_to_int env e1 counter in
+            let (e2, s2, counter) = id_string_to_int env e2 counter in
+            let (e3, s3, counter) = id_string_to_int env e3 counter in
+            (If (m, e1, e2, e3), s1 |> MI.add_seq (MI.to_seq s2) |>  MI.add_seq (MI.to_seq s3), counter)
+        | Let (m, id, e1, e2) ->
+            let (e1, s1, counter) = id_string_to_int env e1 counter in
+            let (id, env2, s2, counter) = (counter, MS.add id counter env, MI.singleton counter id, counter + 1) in
+            let (e2, s3, counter) = id_string_to_int env2 e2 counter in
+            (Let (m, id, e1, e2), s1 |> MI.add_seq (MI.to_seq s2) |>  MI.add_seq (MI.to_seq s3), counter)
+        | Fun (m, id, e) ->
+            let (id, env2, s1, counter) = (counter, MS.add id counter env, MI.singleton counter id, counter + 1) in
+            let (e, s2, counter) = id_string_to_int env2 e counter in
+            (Fun (m, id, e), s1 |> MI.add_seq (MI.to_seq s2), counter)
+        | Ap (m, e1, e2) ->
+            let (e1, s1, counter) = id_string_to_int env e1 counter in
+            let (e2, s2, counter) = id_string_to_int env e2 counter in
+            (Ap (m, e1, e2), s1 |> MI.add_seq (MI.to_seq s2), counter)
+        | LetRecFun (m, id, id2, e1, e2) ->
+            let (id, env2, s1, counter) = (counter, MS.add id counter env, MI.singleton counter id, counter + 1) in
+            let (id2, env3, s2, counter) = (counter, MS.add id2 counter env2, MI.singleton counter id2, counter + 1) in
+            let (e1, s3, counter) = id_string_to_int env3 e1 counter in
+            let (e2, s4, counter) = id_string_to_int env2 e2 counter in
+            (LetRecFun (m, id, id2, e1, e2), s1 |> MI.add_seq (MI.to_seq s2) |>  MI.add_seq (MI.to_seq s3) |>  MI.add_seq (MI.to_seq s4), counter)
+        | EmptyList (m) -> (EmptyList (m), MI.empty, counter)
+        | Cons (m, e1, e2) ->
+            let (e1, s1, counter) = id_string_to_int env e1 counter in
+            let (e2, s2, counter) = id_string_to_int env e2 counter in
+            (Cons (m, e1, e2), s1 |> MI.add_seq (MI.to_seq s2), counter)
+        | Match (m, e1, clauses) ->
+            let (e1, s1, counter) = id_string_to_int env e1 counter in
+            let (clauses, s2, counter) =
+                List.fold_right (fun (pat, e2) (clauses, s2, counter) ->
+                    let (pat, env2, s3, counter) = id_string_to_int_pat pat counter in
+                    let (e2, s4, counter) = id_string_to_int (env |> MS.add_seq (MS.to_seq env2)) e2 counter in
+                    ((pat, e2) :: clauses, s2 |> MI.add_seq (MI.to_seq s3) |> MI.add_seq (MI.to_seq s4), counter)
+                ) clauses ([], MI.empty, counter) in
+            (Match (m, e1, clauses), s1 |> MI.add_seq (MI.to_seq s2), counter)
+
 
 let rec assign_id (e: ('id, 'a) t) (counter: int): (('id, int) t * int) =
     match e with
