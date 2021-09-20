7692aba0521d8fabd86733c1a18e5274176a419f
kgtkr <kgtkr.jp@gmail.com>
2021-09-20 21:58:09 +0900

    impl Type_schema.to_string

--- tklang_compiler/lib/type_scheme.ml
+++ tklang_compiler/lib/type_scheme.ml
@@ -2,4 +2,6 @@ open Set_ext
 
 type t = SI.t * Type.t
 
+let to_string ((vars, t): t): string = "{" ^ (String.concat "," (vars |> SI.to_seq |> List.of_seq |> List.map Int.to_string)) ^ "}" ^ Type.to_string t
+
 let ftv ((vars, t): t): SI.t = SI.diff (Type.ftv t) vars
