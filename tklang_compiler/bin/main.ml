6d1efa920c483c795a185ee3ac64bcf5e1891845
kgtkr <kgtkr.jp@gmail.com>
2021-09-20 23:03:36 +0900

    実行ファイル

--- tklang_compiler/bin/main.ml
+++ tklang_compiler/bin/main.ml
@@ -1,1 +1,19 @@
-let () = print_endline "Hello, World!"
+open Tklang_compiler
+open Tklang_compiler.Map_ext
+
+exception Exception
+
+let () =
+    let src = really_input_string stdin (in_channel_length stdin) in
+    let tokens = Lexer.lex (src |> String.to_seq |> List.of_seq) in
+    let expr = match Parser.expr_parser tokens with
+        | Some (ast, []) -> ast
+        | _ -> raise Exception in
+    let (expr, _) = Expr.assign_id expr 0 in
+    let (expr, _, _) = Expr.id_string_to_int MS.empty expr 0 in
+    let (s, _, _, ets, idents) = Type_equ.pt MI.empty expr 0 in
+    let ets = MI.map (Type_sub.sub_type s) ets in
+    let idents = MI.map (Type_sub.sub_type_scheme s) idents in
+    let out = Expr.to_string ets idents expr in
+    print_string out
+
--- tklang_compiler/lib/type_sub.ml
+++ tklang_compiler/lib/type_sub.ml
@@ -11,4 +11,7 @@ let rec sub_type (s: t) (t: Type.t) = match t with
 
 
 (** 型スキーマの束縛変数は衝突しないという仮定が必要 *)
-let sub_env (s: t) (e: Type_env.t): Type_env.t = MI.map (fun (vars, t) -> (vars, sub_type s t)) e
+let sub_type_scheme (s: t) ((vars, t): Type_scheme.t): Type_scheme.t = (vars, sub_type s t)
+
+(** 型スキーマの束縛変数は衝突しないという仮定が必要 *)
+let sub_env (s: t) (e: Type_env.t): Type_env.t = MI.map (sub_type_scheme s) e
