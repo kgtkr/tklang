2cfce51a20c9db72070f52dd336eb6e3154d5a4b
kgtkr <kgtkr.jp@gmail.com>
2021-09-26 02:15:39 +0900

    Type.ftvのリファクタリング

--- tklang_compiler/lib/type.ml
+++ tklang_compiler/lib/type.ml
@@ -8,12 +8,17 @@ let rec to_string (x:t): string =
     | Func (x, y) -> "(" ^ to_string x ^ " -> " ^ to_string y ^ ")"
     | List x -> "[" ^ to_string x ^ "]"
 
-let rec ftv (ts: t): Type_var_ids.t = match ts with
-    | TypeVar id -> Type_var_ids.singleton id
-    | Bool -> Type_var_ids.empty
-    | Int -> Type_var_ids.empty
-    | Func (t1, t2) -> Type_var_ids.merge (ftv t1) (ftv t2)
-    | List t -> ftv t
+let rec ftv_ (typ: t) (ids: Type_var_ids.t): Type_var_ids.t =
+    match typ with
+    | TypeVar id -> ids |> Type_var_ids.add id
+    | Bool -> ids
+    | Int -> ids
+    | Func (t1, t2) ->
+        let ids = ftv_ t1 ids in
+        ftv_ t2 ids
+    | List t -> ftv_ t ids
+
+let ftv (typ: t): Type_var_ids.t = ftv_ typ Type_var_ids.empty
 
 let rec eq (t1: t) (t2: t): bool = match (t1, t2) with
     | (TypeVar id1, TypeVar id2) -> id1 = id2
